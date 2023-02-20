1c743a0f8d7196eeec0ee3585d7eff4a8248e7b2
Oskarowski <oscar.kacprzak@gmail.com>
2023-02-20 19:07:33 +0100

    -established connection -established disconnect

--- index.js
+++ index.js
@@ -7,3 +7,49 @@ var app = express();
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+app.get("/folder", function (req, res) {
+  res.send("Folder!");
+});
+
+var httpServer = http.createServer(app).listen(8082);
+
+var io = new Server(httpServer, { cors: { origin: "*" } });
+
+const players = [];
+
+function getPlayerIndex(socket) {
+  return socket.id;
+}
+
+io.on("connection", (socket) => {
+  if (Object.keys(players).length == 2) return socket.disconnect(true);
+  console.log("Player ✅ connection", socket.id);
+
+  players[socket.id] = {
+    socket: socket,
+    name: null,
+    ships: [],
+  };
+
+  socket.emit("yourID", socket.id);
+
+  socket.join("game");
+
+  socket.on("greeting", function (data) {
+    players[socket.id].name = data.playerName;
+    console.log("Hello from " + data.playerName);
+  });
+
+  /**
+   *
+   */
+
+  socket.on("disconnect", function (reason) {
+    delete players[socket.id];
+    console.log("Player ❌ disconnected", socket.id);
+  });
+
+  socket.on("fieldClicked", function (fieldElement) {
+    console.log(fieldElement);
+  });
+});
